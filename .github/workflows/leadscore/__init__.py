394906c3bf4fc2a4bc3c9fdab9c77efc6589a780
cobrastormgmail <155102005+cobrastormgmail@users.noreply.github.com>
2025-07-23 18:58:50 +0100

    Delete .github/workflows/leadscore directory

--- .github/workflows/leadscore/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Leadscore function triggered.")
-    return func.HttpResponse("Leadscore API working!", status_code=200)
